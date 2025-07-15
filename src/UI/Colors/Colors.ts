84b6c659a078b68a20a2904f991278887bdac89f
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-07-15 15:10:39 -0500

    Colors saved.

---
+++ src/UI/Colors/Colors.ts
@@ -0,0 +1,33 @@
+//absolute colors
+
+export const white = '#FFF'; //hex code
+export const black = '#000'; //hex code
+
+//Orange shades
+
+export const orange_50 = '#FF9500';
+export const orange_70 = '#FFBF66';
+export const orange_75 = '#FFCA80';
+export const orange_80 = '#FFD499';
+export const orange_90 = '#FFEACC';
+export const orange_95 = '#FFF4E5';
+export const orange_97 = '#FFF9F0';
+export const orange_99 = '#FFFDFA';
+
+//White shades
+
+export const white_90 = '#E4E4E7';
+export const white_95 = '#F1F1F3';
+export const white_97 = '#F7F7F8';
+export const white_99 = '#FCFCFD';
+
+//Grey shades
+
+export const grey_10 = '#191919';
+export const grey_15 = '#262626';
+export const grey_20 = '#333333';
+export const grey_30 = '#4C4C4D';
+export const grey_35 = '#59595A';
+export const grey_40 = '#656567';
+export const grey_60 = '#98989A';
+export const grey_70 = '#B3B3B3';
