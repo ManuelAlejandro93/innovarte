72725c2110cff3bf577b89df24cce23b574174f0
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-07-17 15:18:53 -0500

    BeVietnamPro all font weigths -> Done.

--- src/InnovarteApp.tsx
+++ src/InnovarteApp.tsx
@@ -12,16 +12,10 @@ export const InnovarteApp = () => {
         <h1 className='bg-red-200'>titulo de header</h1>
         <SectionContainer>
           <SingleSectionContainer>
-            <h1 className='bg-app-orange-50'>Titulo de seccion</h1>
-          </SingleSectionContainer>
-          <SingleSectionContainer>
-            <h1>Titulo de seccion</h1>
-          </SingleSectionContainer>
-          <SingleSectionContainer>
-            <h1>Titulo de seccion</h1>
-          </SingleSectionContainer>
-          <SingleSectionContainer>
-            <h1>Titulo de seccion</h1>
+            <h1 className='font-regular'>Example Words</h1>
+            <h1 className='font-medium'>Example Words</h1>
+            <h1 className='font-semibold'>Example Words</h1>
+            <h1 className='font-bold'>Example Words</h1>
           </SingleSectionContainer>
         </SectionContainer>
       </GrandContainer>
