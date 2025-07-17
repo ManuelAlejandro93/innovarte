6c33f29303afb36a980ef07efb76d1a6ad7aa8bd
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-07-17 12:55:16 -0500

    BeVietnamPro font install -> Done.

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import { InnovarteApp } from './InnovarteApp';
 
 import './index.css';
 
+// Importar fuentes
+import '@fontsource/be-vietnam-pro/400.css';
+import '@fontsource/be-vietnam-pro/500.css';
+import '@fontsource/be-vietnam-pro/600.css';
+import '@fontsource/be-vietnam-pro/700.css';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <InnovarteApp></InnovarteApp>
