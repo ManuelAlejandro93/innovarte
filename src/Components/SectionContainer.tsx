8f9068f41a9a25cd50e68bd9a94d050014174f1f
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-07-19 16:40:20 -0500

    section container + px-10

--- src/Components/SectionContainer.tsx
+++ src/Components/SectionContainer.tsx
@@ -2,5 +2,5 @@ import { ChildrenProp } from '@/Interfaces';
 
 //Contenedor de todas las secciones
 export const SectionContainer = ({ children }: ChildrenProp) => {
-  return <div className='grid space-y-16'>{children}</div>;
+  return <div className='grid space-y-16 px-10'>{children}</div>;
 };
