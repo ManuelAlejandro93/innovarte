79474c03e78aca27d188cf1383445b18a10ab732
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-07-19 18:10:28 -0500

    Hay que hacer las vistas responsive.

--- src/InnovarteApp.tsx
+++ src/InnovarteApp.tsx
@@ -1,12 +1,5 @@
-import {
-  GrandContainer,
-  SingleSectionContainer,
-  AppContainer,
-  SectionContainer,
-  Header
-} from './Components';
+import { GrandContainer, AppContainer, Header } from './Components';
 
-import { PhoneIcon } from '@heroicons/react/24/solid';
 export const InnovarteApp = () => {
   return (
     <AppContainer>
