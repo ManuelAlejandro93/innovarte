cb669db7ce20ab7384ab9d4dc335c59b3f1a0e41
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-07-21 15:36:00 -0500

    voy a cambiar el nombre a header-responsive voy en sm.

--- src/Components/Header/TopBanner.tsx
+++ src/Components/Header/TopBanner.tsx
@@ -4,8 +4,9 @@ export const TopBanner = () => {
   return (
     <div className='grid grid-cols-3 bg-app-orange-50 rounded-sm'>
       <div></div>
-      <div className='flex justify-evenly items-center text-app-white-99'>
-        Free courses sale ends soon. Get it now ⭐ <ArrowRight />
+      <div className='flex justify-evenly items-center text-app-white-99 sm:col-span-3 sm:justify-center sm:p-2'>
+        Free courses sale ends soon. Get it now ⭐{' '}
+        <ArrowRight className='sm:ml-6' />
       </div>
       <div></div>
     </div>
