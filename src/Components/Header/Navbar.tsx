8bd12728415ecf48faba5098ee444378a3db2bc7
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-07-21 17:40:21 -0500

    mobile view -> doing it.

--- src/Components/Header/Navbar.tsx
+++ src/Components/Header/Navbar.tsx
@@ -1,39 +1,39 @@
 export const Navbar = () => {
   return (
-    <nav className='navbar grid grid-cols-2 md:px-5 md:py-2 lg:px-10 lg:py-2'>
+    <nav className='navbar grid grid-cols-2 py-4 md:px-5 md:py-2 lg:px-10 lg:py-2'>
       <div className='section-routes flex items-center md:justify-between lg:justify-between'>
         <img
           src='Logos/Logo-desktop.png'
           alt='company-logo'
-          className='md:w-8 md:h-auto lg:w-10 lg:h-auto xl:w-14 xl:h-auto'
+          className='w-12 md:w-8 md:h-auto lg:w-10 lg:h-auto xl:w-14 xl:h-auto'
         />
         <a
           href=''
-          className='bg-app-white-95 rounded-lg md:py-2 md:px-0 lg:py-2 lg:px-1 xl:p-4'
+          className='bg-app-white-95 rounded-lg hidden md:block md:py-2 md:px-0 lg:py-2 lg:px-1 xl:p-4'
         >
           Home
         </a>
         <a
           href=''
-          className='md:py-2 lg:p-2 xl:p-4'
+          className='hidden md:block md:py-2 lg:p-2 xl:p-4'
         >
           Courses
         </a>
         <a
           href=''
-          className='md:py-2 lg:p-2 xl:p-4'
+          className='hidden md:block md:py-2 lg:p-2 xl:p-4'
         >
           About us
         </a>
         <a
           href=''
-          className='md:py-2 lg:p-2 xl:p-4'
+          className='hidden md:block md:py-2 lg:p-2 xl:p-4'
         >
           Pricing
         </a>
         <a
           href=''
-          className='md:py-2 lg:p-2 xl:p-4'
+          className='hidden md:block md:py-2 lg:p-2 xl:p-4'
         >
           Contact
         </a>
@@ -41,13 +41,13 @@ export const Navbar = () => {
       <div className='flex justify-end'>
         <a
           href=''
-          className='md:p-2 lg:p-2 xl:p-4'
+          className='p-4 md:p-2 lg:p-2 xl:p-4'
         >
           Sign Up
         </a>
         <a
           href=''
-          className='bg-app-orange-50 text-app-white-95 rounded-md md:p-2 lg:p-2 xl:py-4 xl:px-8'
+          className='bg-app-orange-50 text-app-white-95 rounded-md p-4 md:p-2 lg:p-2 xl:py-4 xl:px-8'
         >
           Login
         </a>
