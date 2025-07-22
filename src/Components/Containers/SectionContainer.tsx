08c9cf99c753375ad494d83c94481dc617b5e725
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-07-22 15:38:48 -0500

    starting with hero  section.

--- src/Components/Containers/SectionContainer.tsx
+++ src/Components/Containers/SectionContainer.tsx
@@ -2,5 +2,7 @@ import { ChildrenProp } from '@/Interfaces';
 
 //Contenedor de todas las secciones
 export const SectionContainer = ({ children }: ChildrenProp) => {
-  return <div className='grid space-y-16 px-10'>{children}</div>;
+  return (
+    <div className='grid space-y-16 px-0 md:px-5 lg:px-10'>{children}</div>
+  );
 };
--- src/InnovarteApp.tsx
+++ src/InnovarteApp.tsx
@@ -1,10 +1,18 @@
-import { GrandContainer, AppContainer, Header } from './Components';
+import {
+  GrandContainer,
+  AppContainer,
+  Header,
+  SectionContainer
+} from './Components';
 
 export const InnovarteApp = () => {
   return (
     <AppContainer>
       <GrandContainer>
         <Header />
+        <SectionContainer>
+          <></>
+        </SectionContainer>
       </GrandContainer>
     </AppContainer>
   );
