67772488988fd44ee27bbbc6a35393d5eb7c098b
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-07-28 17:02:35 -0500

    hero section - mobile view - done.

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,42 +1,42 @@
 export const Hero = () => {
   return (
-    <div className='pt-16 space-y-10'>
+    <div className='pt-8 md:pt-16 space-y-5 md:space-y-10'>
       <div className='row1'>
-        <div className='flex justify-center'>
+        <div className='flex justify-center items-center'>
           <img
             className='mr-4'
             src='/Icons/flash-icon.png'
             alt='logo-image'
           />
-          <h2 className='md:text-3xl lg:text-5xl font-bold text-app-grey-15'>
+          <h2 className='text-lg md:text-3xl lg:text-5xl font-bold text-app-grey-15'>
             <span className='text-app-orange-50'>Unlock</span> Your Creative
             Potential
           </h2>
           <img
-            className='rotate-90 relative bottom-6'
+            className='w-6 h-auto md:w-10 rotate-90 relative bottom-6'
             src='/Icons/abstract-lines.png'
             alt='logo-image'
           />
         </div>
       </div>
-      <div className='row2 grid space-y-2'>
-        <p className='text-center md:text-2xl lg:text-4xl text-app-grey-15 font-medium'>
+      <div className='row2 grid space-y-2 px-4 md:p-0'>
+        <p className='text-center text-sm md:text-center md:text-2xl lg:text-4xl text-app-grey-15 font-medium'>
           with Online Desing and Development Courses
         </p>
-        <p className='text-center lg:text-lg'>
+        <p className='text-center text-xs lg:text-lg'>
           learn from industry expert and enhace your skills
         </p>
       </div>
-      <div className='row3 md:text-center md:grid md:grid-cols-2 lg:flex lg:justify-center'>
+      <div className='row3 grid grid-cols-2 text-center md:grid md:grid-cols-2 lg:flex lg:justify-center text-xs lg:text-xl'>
         <a
           href=''
-          className='bg-app-orange-50 text-app-white-95 rounded-md p-2 md:p-2 lg:p-2 xl:py-4 xl:px-8 mr-2 lg:text-xl'
+          className='bg-app-orange-50 text-app-white-95 rounded-md p-2 md:p-2 lg:p-2 xl:py-4 xl:px-8 mr-2'
         >
           Explore
         </a>
         <a
           href=''
-          className='rounded-md p-2 md:p-2 lg:p-2 xl:py-4 xl:px-8 bg-white lg:text-xl'
+          className='rounded-md p-2 md:p-2 lg:p-2 xl:py-4 xl:px-8 bg-white'
         >
           View Pricing
         </a>
