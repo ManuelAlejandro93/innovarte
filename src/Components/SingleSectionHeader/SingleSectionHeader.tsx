bfcbcdc7762ace58bd8976c679c27a6e49a17e41
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-09-27 11:47:58 -0500

    ultimo commit en main

--- src/Components/SingleSectionHeader/SingleSectionHeader.tsx
+++ src/Components/SingleSectionHeader/SingleSectionHeader.tsx
@@ -1,18 +1,26 @@
 import { AppButton } from '@/Components';
 import { fontsizes } from '@/Standars';
 
-export const SingleSectionHeader = () => {
+interface SingleSectionInterface {
+  title: String;
+  subtitle: String;
+}
+
+const loremSubtitle =
+  'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit debitis sequi minima itaque hic sunt odit officia dolorem eveniet iste perspiciatis soluta voluptatum temporibus sapiente nobis perferendis veritatis, voluptates labore.';
+
+export const SingleSectionHeader = ({
+  title,
+  subtitle
+}: SingleSectionInterface) => {
   return (
     <div>
       <h2 className={`${fontsizes.titles} text-app-grey-20 mb-1 font-semibold`}>
-        Benefits
+        {title ?? 'Lorem Title'}
       </h2>
       <div className='grid space-y-4 sm:flex sm:flex-row sm:justify-between'>
         <h3 className={`${fontsizes.general} text-app-grey-30 w-2/3`}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
-          aperiam perspiciatis cumque. Deserunt earum temporibus, quaerat
-          quisquam, dolores fuga laborum perferendis officia similique ipsum
-          voluptatem alias nulla architecto iste tempore.
+          {subtitle ?? loremSubtitle}
         </h3>
         <span className='sm:flex sm:flex-col sm:justify-end'>
           <AppButton text={'View All'}></AppButton>
