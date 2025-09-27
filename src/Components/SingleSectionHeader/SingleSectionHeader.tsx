bbcd99f0d55e9a56a7e7c462ab01419a76995c47
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-09-27 10:16:21 -0500

    Single Section Header - Mobile view - Hecho.

--- src/Components/SingleSectionHeader/SingleSectionHeader.tsx
+++ src/Components/SingleSectionHeader/SingleSectionHeader.tsx
@@ -7,14 +7,16 @@ export const SingleSectionHeader = () => {
       <h2 className={`${fontsizes.titles} text-app-grey-20 mb-1 font-semibold`}>
         Benefits
       </h2>
-      <div className='flex justify-between items-center'>
+      <div className='grid space-y-4 sm:flex sm:flex-row sm:justify-between'>
         <h3 className={`${fontsizes.general} text-app-grey-30 w-2/3`}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
           aperiam perspiciatis cumque. Deserunt earum temporibus, quaerat
           quisquam, dolores fuga laborum perferendis officia similique ipsum
           voluptatem alias nulla architecto iste tempore.
         </h3>
-        <AppButton text={'View All'}></AppButton>
+        <span>
+          <AppButton text={'View All'}></AppButton>
+        </span>
       </div>
     </div>
   );
