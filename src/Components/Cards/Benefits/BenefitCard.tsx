ab4f2281f8df268ce14116be052e117ceab1f539
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-09-29 15:52:24 -0500

    Card without grid - done.

--- src/Components/Cards/Benefits/BenefitCard.tsx
+++ src/Components/Cards/Benefits/BenefitCard.tsx
@@ -3,19 +3,23 @@ import { Bolt } from '@/Icons';
 
 export const BenefitCard = () => {
   return (
-    <div className='w-80 h-80 p-8 bg-green-200 grid space-y-8'>
+    <div className='bg-app-white-90 rounded-md w-80 h-80 p-10 md:p-8 grid grid-cols-1 space-y-2 md:space-y-8 lg:space-y-4'>
       <h3
-        className={`w-full text-right ${fontsizes.titles} font-bold text-app-grey-20`}
+        className={`text-right ${fontsizes.titles} font-bold text-app-grey-20 max-h-max `}
       >
         01
       </h3>
       <div className={`flex flex-col justify-around ${fontsizes.general}`}>
         <p className={`text-app-grey-20 font-semibold ${fontsizes.cardTitles}`}>
-          Flexible Learing Schedule
+          Flexible Learning Schedule
         </p>
-        <p className={`text-app-grey-30 ${fontsizes.general}`}>lorem 2</p>
+        <p className={`text-app-grey-30 ${fontsizes.general}`}>
+          Fit your coursework around your existing commiments and obligations.
+        </p>
       </div>
-      <Bolt className='size-8' />
+      <div className='w-full flex justify-end max-h-max '>
+        <Bolt className='size-9 md:size-14 lg:size-16 text-app-grey-20' />
+      </div>
     </div>
   );
 };
