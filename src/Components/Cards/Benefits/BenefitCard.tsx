c984c13bc83c30040f47289ed92cf1facc101d22
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-10-02 17:02:06 -0500

    benefit card + mobile view + tablet view --- Done.

--- src/Components/Cards/Benefits/BenefitCard.tsx
+++ src/Components/Cards/Benefits/BenefitCard.tsx
@@ -3,7 +3,7 @@ import { Bolt as BoltIcon } from '@/Icons';
 
 export const BenefitCard = () => {
   return (
-    <div className='bg-app-white-90 w-full h-[340px] md:h-52 lg:h-64 xl:h-96 rounded-2xl p-10 md:p-8 grid grid-cols-1 space-y-2 md:space-y-8 lg:space-y-4 duration-500 hover:duration-500 hover:transition-all hover:bg-app-orange-50'>
+    <div className='bg-app-white-90 w-full h-[340px] md:h-52 lg:h-64 xl:h-96 rounded-2xl p-10 md:p-2 lg:p-8 grid grid-cols-1 space-y-2 md:space-y-2 lg:space-y-4 duration-500 hover:duration-500 hover:transition-all hover:bg-app-orange-50'>
       <h3
         className={`text-right ${fontsizes.titles} font-bold text-app-grey-20 max-h-max`}
       >
@@ -19,7 +19,7 @@ export const BenefitCard = () => {
       </div>
 
       <div className='w-full flex justify-end max-h-max'>
-        <BoltIcon className='size-9 md:size-14 lg:size-16 text-app-grey-20' />
+        <BoltIcon className='size-9 md:size-10 lg:size-16 text-app-grey-20' />
       </div>
     </div>
   );
