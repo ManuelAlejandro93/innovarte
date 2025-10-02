0943ce7869008671bfeb4b6170d8d45e65b2f411
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-10-02 16:42:46 -0500

    benefit card + legacy benefit card - Done.

--- src/Components/Cards/Benefits/BenefitCard.tsx
+++ src/Components/Cards/Benefits/BenefitCard.tsx
@@ -1,11 +1,11 @@
 import { fontsizes } from '@/Standars';
-import { Bolt } from '@/Icons';
+import { Bolt as BoltIcon } from '@/Icons';
 
 export const BenefitCard = () => {
   return (
-    <div className='bg-app-white-90 rounded-2xl md:w-45 md:h-45 lg:w-80 lg:h-80 p-10 md:p-8 grid grid-cols-1 space-y-2 md:space-y-8 lg:space-y-4 duration-500 hover:duration-500 hover:transition-all hover:bg-app-orange-50'>
+    <div className='bg-app-white-90 w-full h-[340px] md:h-52 lg:h-64 xl:h-96 rounded-2xl p-10 md:p-8 grid grid-cols-1 space-y-2 md:space-y-8 lg:space-y-4 duration-500 hover:duration-500 hover:transition-all hover:bg-app-orange-50'>
       <h3
-        className={`text-right ${fontsizes.titles} font-bold text-app-grey-20 max-h-max `}
+        className={`text-right ${fontsizes.titles} font-bold text-app-grey-20 max-h-max`}
       >
         01
       </h3>
@@ -17,8 +17,35 @@ export const BenefitCard = () => {
           Fit your coursework around your existing commiments and obligations.
         </p>
       </div>
+
+      <div className='w-full flex justify-end max-h-max'>
+        <BoltIcon className='size-9 md:size-14 lg:size-16 text-app-grey-20' />
+      </div>
+    </div>
+  );
+};
+
+export const BenefitCardLegacy = () => {
+  return (
+    <div className='bg-app-white-90 rounded-2xl md:w-45 md:h-45 lg:w-80 lg:h-80 p-10 md:p-8 grid grid-cols-1 space-y-2 md:space-y-8 lg:space-y-4 duration-500 hover:duration-500 hover:transition-all hover:bg-app-orange-50'>
+      <h3
+        className={`text-right ${fontsizes.titles} font-bold text-app-grey-20 max-h-max `}
+      >
+        01
+      </h3>
+
+      <div className={`flex flex-col justify-around ${fontsizes.general}`}>
+        <p className={`text-app-grey-20 font-semibold ${fontsizes.cardTitles}`}>
+          Flexible Learning Schedule
+        </p>
+
+        <p className={`text-app-grey-30 ${fontsizes.general}`}>
+          Fit your coursework around your existing commiments and obligations.
+        </p>
+      </div>
+
       <div className='w-full flex justify-end max-h-max '>
-        <Bolt className='size-9 md:size-14 lg:size-16 text-app-grey-20' />
+        <BoltIcon className='size-9 md:size-14 lg:size-16 text-app-grey-20' />
       </div>
     </div>
   );
--- src/Components/Grids/BenefitGrid.tsx
+++ src/Components/Grids/BenefitGrid.tsx
@@ -1,7 +1,9 @@
+import { BenefitCard } from '@/Components';
+
 export const BenefitGrid = () => {
   return (
-    <div className='bg-slate-400 w-full grid grid-cols-1 grid-rows-6 md:grid-cols-3 md:grid-rows-2 gap-6'>
-      <div className='bg-red-200 w-full h-[340px] md:h-52 lg:h-64 xl:h-96'></div>
+    <div className='bg-red-300 w-full grid grid-cols-1 grid-rows-6 md:grid-cols-3 md:grid-rows-2 gap-6'>
+      <BenefitCard></BenefitCard>
     </div>
   );
 };
