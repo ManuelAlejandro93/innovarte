b79a9dad2c3691cbcf7c8d1ecca744188428de82
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-10-02 17:21:36 -0500

    BenefitSingleCard || Mobile view + Tablet view + Laptop view + Desktop view || Done.

--- src/Components/Grids/BenefitGrid.tsx
+++ src/Components/Grids/BenefitGrid.tsx
@@ -2,7 +2,12 @@ import { BenefitCard } from '@/Components';
 
 export const BenefitGrid = () => {
   return (
-    <div className='bg-red-300 w-full grid grid-cols-1 grid-rows-6 md:grid-cols-3 md:grid-rows-2 gap-6'>
+    <div className='w-full grid grid-cols-1 grid-rows-6 md:grid-cols-3 md:grid-rows-2 gap-6'>
+      <BenefitCard></BenefitCard>
+      <BenefitCard></BenefitCard>
+      <BenefitCard></BenefitCard>
+      <BenefitCard></BenefitCard>
+      <BenefitCard></BenefitCard>
       <BenefitCard></BenefitCard>
     </div>
   );
