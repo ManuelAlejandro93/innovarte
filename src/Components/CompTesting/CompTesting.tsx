a03b234adf8aef6d15fcb9a6b3e39560649d6e3a
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-10-03 17:32:39 -0500

    OurCourseSingleCard + OurCourseGrid || Only sizwe || Mobile view + Tablet view + Laptop view + Desktop view || Done.

--- src/Components/CompTesting/CompTesting.tsx
+++ src/Components/CompTesting/CompTesting.tsx
@@ -10,7 +10,11 @@ export const CompTesting = () => {
         subtitle={loremGeneral}
         viewAllButton={<AppButton text={'View All'} />}
       >
-        <></>
+        {/* //!"Our Courses" Grid */}
+        <div className='grid grid-cols-1 grid-rows-6 gap-6 md:grid-cols-2 md:grid-rows-3 md:gap-4 lg:gap-6'>
+          <div className='bg-red-300 w-full h-96 lg:h-[520px]'></div>
+        </div>
+        {/* //!"Our Courses" Grid */}
       </SingleSection>
     </div>
   );
