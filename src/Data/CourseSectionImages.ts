6e0759cd9f3890586b2ff6f489393684688aa679
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-10-03 12:34:55 -0500

    courses images -- already on cloudinary -- downloadable

---
+++ src/Data/CourseSectionImages.ts
@@ -0,0 +1,14 @@
+export const courseSectionImages = {
+  image1:
+    'https://res.cloudinary.com/dkr08foul/image/upload/v1759511655/Imagen1_suqirj.png',
+  image2:
+    'https://res.cloudinary.com/dkr08foul/image/upload/v1759511890/image2_u2n0ap.png',
+  image3:
+    'https://res.cloudinary.com/dkr08foul/image/upload/v1759511892/Image3_aevrfa.png',
+  image4:
+    'https://res.cloudinary.com/dkr08foul/image/upload/v1759511892/Image4_xok2e3.png',
+  image5:
+    'https://res.cloudinary.com/dkr08foul/image/upload/v1759511892/Image5_n3xnei.png',
+  image6:
+    'https://res.cloudinary.com/dkr08foul/image/upload/v1759511892/Image6_rnyapc.png'
+};
