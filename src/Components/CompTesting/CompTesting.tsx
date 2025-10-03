3031e91adee14c172493fac17da7a73da25c6318
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-10-03 15:50:13 -0500

    Empezando con los courses again x2.

--- src/Components/CompTesting/CompTesting.tsx
+++ src/Components/CompTesting/CompTesting.tsx
@@ -1,7 +1,17 @@
+import { SingleSection, AppButton, BenefitGrid } from '@/Components';
+import { loremGeneral } from '@/Standars';
+
 export const CompTesting = () => {
   return (
     <div>
-      <span className='text-red-500'>Testing Component</span>
+      <p className='text-red-500 text-3xl'>---In Process Component---</p>
+      <SingleSection
+        title={'Our Courses'}
+        subtitle={loremGeneral}
+        viewAllButton={<AppButton text={'View All'} />}
+      >
+        <></>
+      </SingleSection>
     </div>
   );
 };
--- src/Data/BenefitInfo.ts
+++ src/Data/BenefitInfo.ts
@@ -18,56 +18,19 @@ export const BenefitInfo: BenefitInfoInterface[] = [
   },
   {
     benefitTitle: 'Flexible Learning Schedule',
+
     benefitDescription:
       'Fit your coursework around your existing commiments and obligations.'
   },
   {
     benefitTitle: 'Flexible Learning Schedule',
+
     benefitDescription:
       'Fit your coursework around your existing commiments and obligations.'
   },
   {
     benefitTitle: 'Flexible Learning Schedule',
-    benefitDescription:
-      'Fit your coursework around your existing commiments and obligations.'
-  },
-  {
-    benefitTitle: 'Flexible Learning Schedule',
-    benefitDescription:
-      'Fit your coursework around your existing commiments and obligations.'
-  },
-  {
-    benefitTitle: 'Flexible Learning Schedule',
-    benefitDescription:
-      'Fit your coursework around your existing commiments and obligations.'
-  },
-  {
-    benefitTitle: 'Flexible Learning Schedule',
-    benefitDescription:
-      'Fit your coursework around your existing commiments and obligations.'
-  },
-  {
-    benefitTitle: 'Flexible Learning Schedule',
-    benefitDescription:
-      'Fit your coursework around your existing commiments and obligations.'
-  },
-  {
-    benefitTitle: 'Flexible Learning Schedule',
-    benefitDescription:
-      'Fit your coursework around your existing commiments and obligations.'
-  },
-  {
-    benefitTitle: 'Flexible Learning Schedule',
-    benefitDescription:
-      'Fit your coursework around your existing commiments and obligations.'
-  },
-  {
-    benefitTitle: 'Flexible Learning Schedule',
-    benefitDescription:
-      'Fit your coursework around your existing commiments and obligations.'
-  },
-  {
-    benefitTitle: 'Flexible Learning Schedule',
+
     benefitDescription:
       'Fit your coursework around your existing commiments and obligations.'
   }
