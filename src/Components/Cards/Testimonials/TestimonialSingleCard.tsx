f239a127bba0ef09a568dbe9e16bdaff976ff501
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-10-14 17:22:08 -0500

    Testimonials -> SingleCard -> Ready

--- src/Components/Cards/Testimonials/TestimonialSingleCard.tsx
+++ src/Components/Cards/Testimonials/TestimonialSingleCard.tsx
@@ -1,27 +1,24 @@
-import { BoltIcon } from 'lucide-react';
 import { AppButton, AppGeneralText } from '@/Components';
 
 export const TestimonialSingleCard = () => {
   return (
-    <>
-      <div className='rounded-lg border-2 border-app-white-90 w-full h-[250px] md:h-[200px] lg:h-[250px] xl:h-[300px] p-4 md:p-6 lg:p-8 xl:p-12 flex flex-col justify-around'>
-        <AppGeneralText />
-        <div className='testimonial-botton-div-container flex justify-between items-center'>
-          <div className='flex items-center'>
-            <img
-              src='https://res.cloudinary.com/dkr08foul/image/upload/v1760477803/Photo2_qxph1t.png'
-              alt='photo1'
-              className='w-14 h-auto md:w-12 lg:w-16 xl:w-20'
-            />
-            <AppGeneralText
-              className='ml-2'
-              text='Sarah Lee'
-            />
-          </div>
-          <AppButton text={'Read Full Story'}></AppButton>
+    <div className='rounded-lg border-2 border-app-white-90 w-full h-[200px] md:h-[230px] lg:h-[280px] xl:h-[360px] p-4 md:p-6 lg:p-8 xl:p-12 flex flex-col justify-around'>
+      <AppGeneralText />
+      <div className='testimonial-botton-div-container flex justify-between items-center'>
+        <div className='flex items-center'>
+          <img
+            src='https://res.cloudinary.com/dkr08foul/image/upload/v1760477803/Photo2_qxph1t.png'
+            alt='photo1'
+            className='w-14 h-auto md:w-12 lg:w-16 xl:w-20'
+          />
+          <AppGeneralText
+            className='ml-2'
+            text='Sarah Lee'
+          />
         </div>
+        <AppButton text={'Read Full Story'}></AppButton>
       </div>
-    </>
+    </div>
   );
 };
 
