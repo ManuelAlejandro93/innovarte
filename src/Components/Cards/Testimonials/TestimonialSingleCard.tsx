7cffd6c3ff7c003115e5a81a8ab2f62042e17c5f
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-10-14 15:27:35 -0500

    Testimonials -> SingleCard -> Size for card - Found, viewbyView.

--- src/Components/Cards/Testimonials/TestimonialSingleCard.tsx
+++ src/Components/Cards/Testimonials/TestimonialSingleCard.tsx
@@ -1,5 +1,7 @@
 export const TestimonialSingleCard = () => {
   return (
-    <div className='rounded-lg border-2 border-app-white-90 w-full h-[200px] lg:h-[200px] xl:h-[200px] p-4 md:p-6 lg:p-8 xl:p-12 grid gap-4 md:gap-6 lg:gap-4 bg-lime-200'></div>
+    <>
+      <div className='rounded-lg border-2 border-app-white-90 w-full h-[250px] md:h-[200px] lg:h-[250px] xl:h-[300px] p-4 md:p-6 lg:p-8 xl:p-12 grid gap-4 md:gap-6 lg:gap-4 bg-lime-200 md:bg-red-100'></div>
+    </>
   );
 };
