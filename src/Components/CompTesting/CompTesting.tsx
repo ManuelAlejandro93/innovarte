b13bc8be576e967a94dc4f1b1291dfa4cdffb530
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-10-15 11:45:30 -0500

    pricing header -> Grid Container - Done -> Single Card - Container -> Done.

--- src/Components/CompTesting/CompTesting.tsx
+++ src/Components/CompTesting/CompTesting.tsx
@@ -1,4 +1,4 @@
-import { loremGeneral } from '@/Standars';
+import { loremGeneral, fontsizes } from '@/Standars';
 import { SingleSection } from '@/Components';
 
 export const CompTesting = () => {
@@ -9,7 +9,26 @@ export const CompTesting = () => {
         title={'Pricing'}
         subtitle={loremGeneral}
       >
-        <></>
+        {/* //!Container */}
+        <div className='bg-yellow-200 flex p-4 md:p-6 lg:p-8 xl:p-12 gap-6 md:gap-8 lg:gap-10 xl:gap-14 flex-col md:flex-row'>
+          <div className='w-full h-fit grid gap-4 bg-orange-400'>
+            <h1 className={`${fontsizes.general} text-center`}>oee</h1>
+            <h1 className={`${fontsizes.general} text-center`}>oee</h1>
+            <h1 className={`${fontsizes.general} text-center`}>oee</h1>
+            <h1 className={`${fontsizes.general} text-center`}>oee</h1>
+            <h1 className={`${fontsizes.general} text-center`}>oee</h1>
+            <h1 className={`${fontsizes.general} text-center`}>oee</h1>
+          </div>
+          <div className='w-full h-fit grid gap-4 bg-orange-400'>
+            <h1 className={`${fontsizes.general} text-center`}>oee</h1>
+            <h1 className={`${fontsizes.general} text-center`}>oee</h1>
+            <h1 className={`${fontsizes.general} text-center`}>oee</h1>
+            <h1 className={`${fontsizes.general} text-center`}>oee</h1>
+            <h1 className={`${fontsizes.general} text-center`}>oee</h1>
+            <h1 className={`${fontsizes.general} text-center`}>oee</h1>
+          </div>
+        </div>
+        {/* //!Container */}
       </SingleSection>
     </div>
   );
