3cb14ffc4025f15ffdbe0f16bcda9dc90237435a
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-10-19 17:41:22 -0500

    Header&&Hero&&Benefits&&Courses -> Done.

--- src/Components/Cards/Testimonials/TestimonialSingleCard.tsx
+++ src/Components/Cards/Testimonials/TestimonialSingleCard.tsx
@@ -2,8 +2,11 @@ import { AppButton, AppGeneralText } from '@/Components';
 
 export const TestimonialSingleCard = () => {
   return (
-    <div className='rounded-lg border-2 border-app-white-90 w-full h-[200px] md:h-[230px] lg:h-[280px] xl:h-[360px] p-4 md:p-6 lg:p-8 xl:p-12 flex flex-col justify-around'>
-      <AppGeneralText />
+    <div className='rounded-lg border-2 border-app-white-90 w-full h-[200px] md:h-[230px] lg:h-[280px] xl:h-[360px] p-4 md:p-6 lg:p-8 xl:p-12 flex flex-col justify-around bg-app-blue-95'>
+      <AppGeneralText
+        className='italic'
+        text='"Fit your coursework around your existing commiments and obligations."'
+      />
       <div className='testimonial-botton-div-container flex justify-between items-center'>
         <div className='flex items-center'>
           <img
@@ -11,10 +14,13 @@ export const TestimonialSingleCard = () => {
             alt='photo1'
             className='w-14 h-auto md:w-12 lg:w-16 xl:w-20'
           />
-          <AppGeneralText
-            className='ml-2'
-            text='Sarah Lee'
-          />
+          <div className='ml-2'>
+            <AppGeneralText text='Sarah Lee' />
+            <AppGeneralText
+              className='font-bold text-app-blue-50'
+              text='Civil Engineering Student.'
+            />
+          </div>
         </div>
         <AppButton text={'Read Full Story'}></AppButton>
       </div>
