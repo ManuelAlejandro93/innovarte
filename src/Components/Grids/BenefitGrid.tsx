2e987030e82ab8a498dd9d0e818c2258d31f0c00
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-10-20 12:53:19 -0500

    Benefit Cards - Hecho

--- src/Components/Grids/BenefitGrid.tsx
+++ src/Components/Grids/BenefitGrid.tsx
@@ -1,17 +1,7 @@
-import { BenefitCard } from '@/Components';
-import { BenefitInfo } from '@/Data';
-
-export const BenefitGrid = () => {
+export const BenefitGrid = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='w-full grid grid-cols-1 grid-rows-6 md:grid-cols-3 md:grid-rows-2 gap-6'>
-      {BenefitInfo.map((singleBenefit, i) => (
-        <BenefitCard
-          key={`${i + 1}_${singleBenefit.benefitTitle}`}
-          benefitNumber={i + 1}
-          benefitTitle={singleBenefit.benefitTitle}
-          benefitDescription={singleBenefit.benefitDescription}
-        ></BenefitCard>
-      ))}
+      {children}
     </div>
   );
 };
--- src/Sections/Benefits.tsx
+++ src/Sections/Benefits.tsx
@@ -1,5 +1,7 @@
 import { SingleSection, AppButton, BenefitGrid } from '@/Components';
 import { loremGeneral } from '@/Standars';
+import { BenefitCard } from '@/Components';
+import { BenefitInfo } from '@/Data';
 
 export const Benefits = () => {
   return (
@@ -8,7 +10,16 @@ export const Benefits = () => {
       subtitle={loremGeneral}
       viewAllButton={<AppButton text={'View All'} />}
     >
-      <BenefitGrid />
+      <BenefitGrid>
+        {BenefitInfo.map((singleBenefit, i) => (
+          <BenefitCard
+            key={`${i + 1}_${singleBenefit.benefitTitle}`}
+            benefitNumber={i + 1}
+            benefitTitle={singleBenefit.benefitTitle}
+            benefitDescription={singleBenefit.benefitDescription}
+          ></BenefitCard>
+        ))}
+      </BenefitGrid>
     </SingleSection>
   );
 };
