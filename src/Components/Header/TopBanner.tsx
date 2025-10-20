1e29b440c6fe9b121ff61c734f1f4c37a9223b64
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-10-20 12:16:46 -0500

    Subdividiendo los componentes.

--- src/Components/Header/TopBanner.tsx
+++ src/Components/Header/TopBanner.tsx
@@ -7,7 +7,7 @@ export const TopBanner = () => {
       className={`flex w-full bg-app-blue-50 rounded-sm p-2 ${fontsizes.general}`}
     >
       <div className='w-full flex items-center text-app-white-99 justify-center'>
-        Free courses sale ends soon. Get it now ⭐
+        Preparate con Nosotros y entra a la U ⭐
         <ArrowRight className='ml-2 md:ml-4' />
       </div>
     </div>
