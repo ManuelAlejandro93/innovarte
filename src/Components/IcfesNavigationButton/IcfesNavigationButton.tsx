3a0604b16e05c2f6b9843d5ecac07197e752d5be
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-10-27 12:54:06 -0500

    icfesNavigationButton - random update.

--- src/Components/IcfesNavigationButton/IcfesNavigationButton.tsx
+++ src/Components/IcfesNavigationButton/IcfesNavigationButton.tsx
@@ -1,3 +1,21 @@
-export const IcfesNavigationButton = () => {
-  return <div>IcfesNavigationButton</div>;
+import { AppButtonInterface as IcfesNavigationButtonInterface } from '@/Interfaces';
+import { fontsizes } from '@/Standars';
+import { grey_15 } from '@/UI';
+
+export const IcfesNavigationButton = ({
+  className,
+  text,
+  backgroud_color,
+  url
+}: IcfesNavigationButtonInterface) => {
+  return (
+    <a
+      href={`${url ?? 'https://www.icfes.gov.co/'}`}
+      className={`${backgroud_color ? backgroud_color : 'bg-app-yellow-80'} ${
+        fontsizes.general
+      } text-${grey_15} rounded-lg p-4 md:p-2 lg:p-4 hover:cursor-pointer ${className}`}
+    >
+      {text}
+    </a>
+  );
 };
