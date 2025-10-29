5a0da8660cd6a49ab27f0ed606decd802bd680ff
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-10-29 13:18:37 -0500

    installing store

--- src/Store/Slices/TestSlice.ts
+++ src/Store/Slices/TestSlice.ts
@@ -1,1 +1,14 @@
-export const testSlice = 0;
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  state: 0
+};
+
+export const testSlice = createSlice({
+  name: 'auth-state',
+  initialState,
+  reducers: {}
+});
+
+export const testReducer = testSlice.reducer;
+export const {} = testSlice.actions;
--- src/Store/Store/Store.ts
+++ src/Store/Store/Store.ts
@@ -1,1 +1,9 @@
-export const store = 0;
+import { configureStore } from '@reduxjs/toolkit';
+import { testReducer } from '../Slices';
+
+export const store = configureStore({
+  reducer: { testReducer }
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
