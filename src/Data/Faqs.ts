85000c9abaad10f3adbeb6a9f3302f06d113f9a8
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-10-30 11:38:48 -0500

    redux-slices -> benefits|courses|testimonials -> done -> faq - more data were added.

--- src/Data/Faqs.ts
+++ src/Data/Faqs.ts
@@ -21,5 +21,69 @@ export const FAQs: FAQSingleQuestionCardInterface[] = [
     answer:
       'No. El simulacro está diseñado para ser lo más real posible. Por lo que no se podrá llegar tarde.',
     isShown: true
+  },
+  {
+    question: '¿El curso puede pagarse a crédito?',
+
+    answer: 'Puede pagarse en máximo 3 coutas mensuales.',
+
+    isShown: false
+  },
+
+  {
+    question: '¿Las cuotas deben ser del mismo valor?',
+
+    answer: 'Sí. Las 3 cuotas son de 150000 pesos cada una.',
+
+    isShown: true
+  },
+
+  {
+    question: '¿Hay descuento si tengo 2 hijos estudiando?',
+
+    answer: 'Sí. Hay un descuento del 5% en la segunda matrícula.',
+
+    isShown: true
+  },
+
+  {
+    question: '¿Puedo llegar tarde a los simulacros?',
+
+    answer:
+      'No. El simulacro está diseñado para ser lo más real posible. Por lo que no se podrá llegar tarde.',
+
+    isShown: true
+  },
+  {
+    question: '¿El curso puede pagarse a crédito?',
+
+    answer: 'Puede pagarse en máximo 3 coutas mensuales.',
+
+    isShown: false
+  },
+
+  {
+    question: '¿Las cuotas deben ser del mismo valor?',
+
+    answer: 'Sí. Las 3 cuotas son de 150000 pesos cada una.',
+
+    isShown: true
+  },
+
+  {
+    question: '¿Hay descuento si tengo 2 hijos estudiando?',
+
+    answer: 'Sí. Hay un descuento del 5% en la segunda matrícula.',
+
+    isShown: true
+  },
+
+  {
+    question: '¿Puedo llegar tarde a los simulacros?',
+
+    answer:
+      'No. El simulacro está diseñado para ser lo más real posible. Por lo que no se podrá llegar tarde.',
+
+    isShown: true
   }
 ];
