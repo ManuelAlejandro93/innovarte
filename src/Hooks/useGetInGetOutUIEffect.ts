96281056c8365e3432984e52219fa480b9116e24
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-11-02 14:33:21 -0500

    testimonial-single-card-ui-effect - update

--- src/Hooks/useGetInGetOutUIEffect.ts
+++ src/Hooks/useGetInGetOutUIEffect.ts
@@ -1,3 +1,12 @@
+import { useState } from 'react';
+
 export const useGetInGetOutUIEffect = () => {
+  const [uiEffectDynamicClass, setUiEffectDynamicClass] = useState<string>('');
+
+  const [orderForEffectToggle, setOrderForEffectToggle] =
+    useState<boolean>(false);
+
+  const [renderingFullTestimonialToggle, setRenderingFullTestimonialToggle] =
+    useState<boolean>(false);
   return {};
 };
