99a17c3645e193eedf64c1447e0559af3ccab45f
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-11-02 17:41:05 -0500

    commit de rescate.

--- src/Data/CourseSectionData.ts
+++ src/Data/CourseSectionData.ts
@@ -66,5 +66,116 @@ export const courseSectionData: CourseSectionDataInterface[] = [
     subtitle:
       'Aprende Sociales con el profe ganador del Premio Nobel de Sociales.',
     icfes_subject_img_url: icfesLinks.socialScience
+  },
+  {
+    url: 'https://res.cloudinary.com/dkr08foul/image/upload/v1759511655/Imagen1_suqirj.png',
+
+    img_description: 'image1',
+
+    intensity: '10 Clases',
+
+    area: 'Ciencias Puras',
+
+    teacher: 'Juan Pérez.',
+
+    title: 'Matemáticas',
+
+    subtitle:
+      'Aprende Matemática con el profe ganador del Premio Eistein de las Matemáticas.',
+
+    icfes_subject_img_url: icfesLinks.math
+  },
+
+  {
+    url: 'https://res.cloudinary.com/dkr08foul/image/upload/v1759511890/image2_u2n0ap.png',
+
+    img_description: 'image2',
+
+    intensity: '4 Clases',
+
+    area: 'Humanidades.',
+
+    teacher: 'Ronaldo Gómez',
+
+    title: 'Inglés',
+
+    subtitle: 'Aprende Inglés con el profe ganador del Premio Nobel de Inglés.',
+
+    icfes_subject_img_url: icfesLinks.english
+  },
+
+  {
+    url: 'https://res.cloudinary.com/dkr08foul/image/upload/v1759511892/Image3_aevrfa.png',
+
+    img_description: 'image3',
+
+    intensity: '3 Clases',
+
+    area: 'Ciencias Básicas',
+
+    teacher: 'Cristian Ibargüen',
+
+    title: 'Química',
+
+    subtitle:
+      'Aprende Química con el profe ganador del Premio Nobel de Química.',
+
+    icfes_subject_img_url: icfesLinks.natureSciences
+  },
+
+  {
+    url: 'https://res.cloudinary.com/dkr08foul/image/upload/v1759511892/Image4_xok2e3.png',
+
+    img_description: 'image4',
+
+    intensity: '3 Clases',
+
+    area: 'Ciencias Básicas',
+
+    teacher: 'Manuel Pinzón',
+
+    title: 'Física',
+
+    subtitle: 'Aprende Física con el profe ganador del Premio Nobel de Física.',
+
+    icfes_subject_img_url: icfesLinks.natureSciences
+  },
+
+  {
+    url: 'https://res.cloudinary.com/dkr08foul/image/upload/v1759511892/Image5_n3xnei.png',
+
+    img_description: 'image5',
+
+    intensity: '3 Clases',
+
+    area: 'Ciencias Básicas',
+
+    teacher: 'Antonia  Álvares',
+
+    title: 'Literatura',
+
+    subtitle:
+      'Aprende Literatura con la profe ganadora del Premio Nobel de Literatura.',
+
+    icfes_subject_img_url: icfesLinks.criticReading
+  },
+
+  {
+    url: 'https://res.cloudinary.com/dkr08foul/image/upload/v1759511892/Image6_rnyapc.png',
+
+    img_description: 'image6',
+
+    intensity: '3 Clases',
+
+    area: 'Ciencias Básicas',
+
+    teacher: 'Hector Mora',
+
+    title: 'Sociales',
+
+    subtitle:
+      'Aprende Sociales con el profe ganador del Premio Nobel de Sociales.',
+
+    icfes_subject_img_url: icfesLinks.socialScience
   }
 ];
