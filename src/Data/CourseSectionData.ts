eda3c194a50be959c4435a29b173470a3f3ab35c
Manuel Alejandro Pinzón Aguire <clasefisica2019@gmail.com>
2025-11-06 12:25:47 -0500

    update.

--- src/Data/CourseSectionData.ts
+++ src/Data/CourseSectionData.ts
@@ -6,7 +6,7 @@ export const courseSectionData: CourseSectionDataInterface[] = [
     url: 'https://res.cloudinary.com/dkr08foul/image/upload/v1759511655/Imagen1_suqirj.png',
     img_description: 'image1',
     intensity: '10 Clases',
-    area: 'Ciencias Puras',
+    area: 'Matemáticas',
     teacher: 'Juan Pérez.',
     title: 'Matemáticas',
     subtitle:
@@ -28,7 +28,7 @@ export const courseSectionData: CourseSectionDataInterface[] = [
     url: 'https://res.cloudinary.com/dkr08foul/image/upload/v1759511892/Image3_aevrfa.png',
     img_description: 'image3',
     intensity: '3 Clases',
-    area: 'Ciencias Básicas',
+    area: 'Ciencias',
     teacher: 'Cristian Ibargüen',
     title: 'Química',
     subtitle:
@@ -39,7 +39,7 @@ export const courseSectionData: CourseSectionDataInterface[] = [
     url: 'https://res.cloudinary.com/dkr08foul/image/upload/v1759511892/Image4_xok2e3.png',
     img_description: 'image4',
     intensity: '3 Clases',
-    area: 'Ciencias Básicas',
+    area: 'Ciencias',
     teacher: 'Manuel Pinzón',
     title: 'Física',
     subtitle: 'Aprende Física con el profe ganador del Premio Nobel de Física.',
@@ -49,7 +49,7 @@ export const courseSectionData: CourseSectionDataInterface[] = [
     url: 'https://res.cloudinary.com/dkr08foul/image/upload/v1759511892/Image5_n3xnei.png',
     img_description: 'image5',
     intensity: '3 Clases',
-    area: 'Ciencias Básicas',
+    area: 'Humanidades',
     teacher: 'Antonia  Álvares',
     title: 'Literatura',
     subtitle:
@@ -60,7 +60,7 @@ export const courseSectionData: CourseSectionDataInterface[] = [
     url: 'https://res.cloudinary.com/dkr08foul/image/upload/v1759511892/Image6_rnyapc.png',
     img_description: 'image6',
     intensity: '3 Clases',
-    area: 'Ciencias Básicas',
+    area: 'Humanidades',
     teacher: 'Hector Mora',
     title: 'Sociales',
     subtitle:
@@ -74,7 +74,7 @@ export const courseSectionData: CourseSectionDataInterface[] = [
 
     intensity: '10 Clases',
 
-    area: 'Ciencias Puras',
+    area: 'Matemáticas',
 
     teacher: 'Juan Pérez.',
 
@@ -111,7 +111,7 @@ export const courseSectionData: CourseSectionDataInterface[] = [
 
     intensity: '3 Clases',
 
-    area: 'Ciencias Básicas',
+    area: 'Ciencias',
 
     teacher: 'Cristian Ibargüen',
 
@@ -130,7 +130,7 @@ export const courseSectionData: CourseSectionDataInterface[] = [
 
     intensity: '3 Clases',
 
-    area: 'Ciencias Básicas',
+    area: 'Ciencias',
 
     teacher: 'Manuel Pinzón',
 
@@ -148,7 +148,7 @@ export const courseSectionData: CourseSectionDataInterface[] = [
 
     intensity: '3 Clases',
 
-    area: 'Ciencias Básicas',
+    area: 'Humanidades',
 
     teacher: 'Antonia  Álvares',
 
@@ -167,7 +167,7 @@ export const courseSectionData: CourseSectionDataInterface[] = [
 
     intensity: '3 Clases',
 
-    area: 'Ciencias Básicas',
+    area: 'Humanidades',
 
     teacher: 'Hector Mora',
 
